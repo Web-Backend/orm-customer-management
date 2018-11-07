166cc78737f392c1f3fff54c86f21be84cdeae90
long <longhoang94>
2018-11-07 21:24:38 +0700

    sad commit

---
+++ src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package com.codegym.cms.controller;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/create-customer")
+    public ModelAndView showCreateForm(){
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listCustomer() {
+        List<Customer> customers = customerService.findAll();
+        return new ModelAndView("/customer/list", "customers", customers);
+    }
+
+    @GetMapping("/customer/{id}/edit")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Customer customer = customerService.findById(id);
+        if (customer != null) {
+            return new ModelAndView("/customer/edit", "customer", customer);
+        } else {
+            return new ModelAndView("error-404");
+        }
+    }
+}
