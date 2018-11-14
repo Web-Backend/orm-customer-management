b2108e87b40d15e4e0897a70413e506c8084de1f
longhoang94 <longsutls@gmail.com>
2018-11-13 20:48:39 -0500

    view customer(s) belong to province

--- src/main/java/com/codegym/cms/controller/ProvinceController.java
+++ src/main/java/com/codegym/cms/controller/ProvinceController.java
@@ -1,6 +1,8 @@
 package com.codegym.cms.controller;
 
+import com.codegym.cms.model.Customer;
 import com.codegym.cms.model.Province;
+import com.codegym.cms.service.CustomerService;
 import com.codegym.cms.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class ProvinceController {
     @Autowired
     private ProvinceService provinceService;
+
+    @Autowired
+    private CustomerService customerService;
 
     @GetMapping("/provinces")
     public ModelAndView listProvinces() {
@@ -58,6 +63,20 @@ public class ProvinceController {
         provinceService.remove(province.getId());
         ModelAndView modelAndView = new ModelAndView("/province/list", "provinces",provinceService.findAll());
         modelAndView.addObject("message", "deleted successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/view-province/{id}")
+    public ModelAndView viewProvince(@PathVariable("id") Long id) {
+        Province province = provinceService.findById(id);
+        if (province == null) {
+            return new ModelAndView("/error-404");
+        }
+        Iterable<Customer> customers = customerService.findAllByProvince(province);
+
+        ModelAndView modelAndView = new ModelAndView("/province/view");
+        modelAndView.addObject("province", province);
+        modelAndView.addObject("customers", customers);
         return modelAndView;
     }
 
